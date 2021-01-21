e54710794c6302acb93ef7d4f90707d43a3794c0
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-21 10:53:17 -0800

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
-import Tours from './Tours'
+import Places from './Places'
 
 const url = 'https://google.com/images'
 function App() {
