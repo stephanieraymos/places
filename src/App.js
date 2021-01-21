1f1e2d13caa72f25e3ca5178f120dd4cf8700449
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-21 10:45:48 -0800

    Loading component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://google.com/images'
 function App() {
   return <h2>Places</h2>
 }
