076d3624336cf1ecf41e37e82be212a7c0a5bdef
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-23 15:55:58 -0800

    fetching

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Loading from './Loading'
 import Places from './Places'
 import Data from './places-data'
 
-const url = { Data }
+const data = Data
+
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
 
   const [loading, setLoading] = useState(true);
   const [places, setPlaces] = useState([]);
+
+  const fetchPlaces = async () => {
+    setLoading(true) //make sure loading is set to true just incase
+    const response = await fetch(url);
+    const places = await response.json();
+    console.log(places)
+  }
+
+  useEffect(() => {
+    fetchPlaces();
+  }, [])
 
   if (loading) {
     return (
