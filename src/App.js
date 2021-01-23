339d744ed33c6ad14d684aab29e38c4ae1d5cad2
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-23 15:44:18 -0800

    State and multiple returns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Places from './Places'
+import Data from './places-data'
 
-const url = 'https://google.com/images'
+// const url = 'https://google.com/images'
+const url = { Data }
 function App() {
-  return <h2>Places</h2>
+
+  const [loading, setLoading] = useState(true);
+  const [places, setPlaces] = useState([]);
+
+  if (loading) {
+    return (
+      <main>
+        < Loading />
+      </main>
+    )
+  }
+  return
+  <>
+
+  </>
 }
 
 export default App
