748352093e933e7ca5114295cba5e3c3d865a22f
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-23 16:11:19 -0800

    Read more state

--- src/Place.js
+++ src/Place.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 const Place = ({ id, image, info, price, name }) => {
 
+  const [readMore, setReadMore] = useState(false);
+
   return <article className="single-tour">
     <img src={image} alt={name} />
     <footer>
@@ -9,7 +11,7 @@ const Place = ({ id, image, info, price, name }) => {
         <h4>{name}</h4>
         <h4 className="tour-price">${price}</h4>
       </div>
-      <p>{info}</p>
+      <p>{readMore ? info : `${info.substring(0, 200)}`}...</p>
       <button className="delete-btn">Delete</button>
     </footer>
   </article>;
